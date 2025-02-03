83826fe46bf78372293f666d514c7907e5b19d32
Jafagervik <jorgenafagervik@gmail.com>
2025-02-03 11:12:45 +0100

    more

--- README.md
+++ README.md
@@ -5,3 +5,41 @@ String distance library written in zig
 ## Algorithms
 
 - [x] Hamming distance
+- [ ] Levenshtein distance
+- [ ] Longest common subsequence distance
+- [ ] Jaro-Winkler distance
+
+## Usage
+
+```zig
+const std = @import("std");
+const zdist =  @import("zdist");
+
+pub fn main() !void {
+    const a_str = "abcdef";
+    const b_str = "cdefgh";
+
+    const distance = zdist.lcs_distance(a_str, b_str);
+
+    std.debug.println("Distance between {s} and {s} is: {d}\n", .{a_str, b_str, distance});
+}
+```
+
+## Install (Per 0.14 beta)
+
+Run this command in the parent directory of your project
+
+```sh
+zig fetch --save https://github.com/Jafagervik/zdist/tarball/v0.1.0
+```
+
+Or alternatively use master or another version
+
+Then add these lines to build.zig before b.installArtifact(exe)
+
+```zig
+const zdist = b.dependency("zdist", .{});
+
+exe.root_module.addImport("zdist", zdist.module("zdist"));
+
+```
--- src/zdist.zig
+++ src/zdist.zig
@@ -16,10 +16,62 @@ pub fn hamming_distance(astr: []const u8, bstr: []const u8) !u32 {
     return dist;
 }
 
-test "hamming_distance" {
+test "Hamming distance" {
     const testing = std.testing;
 
     try testing.expectEqual(@as(u32, 3), try hamming_distance("karolin", "kathrin"));
     try testing.expectEqual(@as(u32, 0), try hamming_distance("same", "same"));
     try testing.expectError(error.UnequalLength, hamming_distance("short", "longer"));
+}
+
+pub const JaroWinklerOptions = struct {
+    prefix_scale: f32 = 0.1,
+    prefix_max_length: u32 = 4,
+    boost_threshold: f32 = 0.7,
+};
+
+/// Jaro-winkler distance between two strings with options
+pub fn jaro_winkler_distance(astr: []const u8, bstr: []const u8, options: JaroWinklerOptions) f32 {
+    _ = astr;
+    _ = bstr;
+    return options.boost_threshold * options.prefix_scale * @as(f32, @floatFromInt(options.prefix_max_length));
+}
+
+test "Jaro winkler" {
+    const testing = std.testing;
+
+    const a = "abc";
+    const b = "def";
+
+    const result = jaro_winkler_distance(a, b, .{});
+
+    try testing.expectEqual(0.28, result);
+}
+
+/// Levenshtein distance between two strings
+pub fn levenshtein_distance(astr: []const u8, bstr: []const u8) u32 {
+    _ = astr;
+    _ = bstr;
+    // if (mem.eql(u8, astr, bstr)) return 0;
+    //
+    // const m = astr.len;
+    // const n = bstr.len;
+    //
+    // var matrix = [_][n]u32{[_]u32{0} ** n} ** m;
+    //
+    // for (astr, 0..) |a, i| {
+    //     for (bstr, 0..) |b, j| {
+    //         _ = matrix;
+    //     }
+    // }
+    //
+    // return matrix[m - 1][n - 1];
+}
+
+/// Longest common subsequence algorithm
+pub fn lcs_distance(astr: []const u8, bstr: []const u8) u32 {
+    _ = astr;
+    _ = bstr;
+
+    return 0;
 }
